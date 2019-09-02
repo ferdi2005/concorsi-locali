b651750e0e94c84e1aa78247d49f4c871ff621a1
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-09-02 15:52:20 +0200

    looks for pageids

--- app/jobs/update_data_job.rb
+++ app/jobs/update_data_job.rb
@@ -9,12 +9,11 @@ class UpdateDataJob < ApplicationJob
       photolist = HTTParty.get(request_photolist, uri_adapter: Addressable::URI).to_a
 
       photolist = photolist[1][1]['categorymembers']
-
       photolist.each do |photo|
       unless Photo.find_by(pageid: photo['pageid'])
-          photoinfo = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&titles=#{photo['title']}&prop=imageinfo&iiprop=user|timestamp|userid&format=json", uri_adapter: Addressable::URI).to_a[1][1]['pages'][photo['pageid'].to_s]['imageinfo'][0] # Looks for photoinfo
+          photoinfo = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&pageids=#{photo['pageid']}&prop=imageinfo&iiprop=user|timestamp|userid&format=json", uri_adapter: Addressable::URI).to_a[1][1]['pages'][photo['pageid'].to_s]['imageinfo'][0] # Looks for photoinfo
 
-          globalusage = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&prop=globalusage&titles=#{photo['title']}.jpg&gunamespace=0&format=json", uri_adapter: Addressable::URI).to_a[1][1]['pages']['-1']['globalusage'].empty?
+          globalusage = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&prop=globalusage&pageids=#{photo['pageid']}.jpg&gunamespace=0&format=json", uri_adapter: Addressable::URI).to_a[1][1]['pages']['-1']['globalusage'].empty?
 
           unless @creator = Creator.find_by(username: photoinfo['user'])
             @creator = Creator.create(username: photoinfo['user'], userid: photoinfo['userid'])
