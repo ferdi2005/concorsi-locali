93c4eb2427d4773bb8c26cd71d3f66c0502806f5
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-09-03 11:22:08 +0200

    sentry-raven gem, statistiche sugli utenti iscritti appositamente

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Concorsilocali
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    Raven.configure do |config|
+      config.dsn = 'https://272aa322b1634af78c776ce630737707:4cbde5d0d32a45a18c1227ef6432a280@sentry.io/1549895'
+    end
   end
 end
