bb180e8d78215beee8c395bf8da551ac728ef850
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-09-08 20:21:23 +0200

    Evito chiamata api inutile

--- app/jobs/update_data_job.rb
+++ app/jobs/update_data_job.rb
@@ -25,10 +25,10 @@ class UpdateDataJob < ApplicationJob
         photolist.each do |photo|
           unless Photo.find_by(pageid: photo['pageid'])
               photoinfo = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&pageids=#{photo['pageid']}&prop=imageinfo&iiprop=user|timestamp|userid&format=json", uri_adapter: Addressable::URI).to_a[1][1]['pages'][photo['pageid'].to_s]['imageinfo'][0] # Looks for photoinfo
-              creationdate = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&meta=globaluserinfo&guiuser=#{photoinfo['user']}&format=json", uri_adapter: Addressable::URI).to_a[1][1]['globaluserinfo']['registration']
               globalusage = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&prop=globalusage&pageids=#{photo['pageid']}&gunamespace=0&format=json", uri_adapter: Addressable::URI).to_a[1][1]['pages'][photo['pageid'].to_s]['globalusage'].try(:empty?)
 
               unless (@creator = Creator.find_by(username: photoinfo['user']))
+                creationdate = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&meta=globaluserinfo&guiuser=#{photoinfo['user']}&format=json", uri_adapter: Addressable::URI).to_a[1][1]['globaluserinfo']['registration']
                 @creator = Creator.create(username: photoinfo['user'], userid: photoinfo['userid'], creationdate: creationdate)
                 @creator.update_attribute(:proveniencecontest, contest.id) if creationdate.to_date == photoinfo['timestamp'].to_date || creationdate.to_date.between?(Date.parse('30/08/2019'), Date.parse('30/09/2019'))
               end
