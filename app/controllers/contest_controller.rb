72efb77729933b403a9b0f8f52e823c6073d0895
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-09-09 15:01:37 +0200

    Richiesta ajax per il conto totale degli utenti partecipanti
    Scopo: ridurre il tempo di caricamento molto alto della pagina.
    
    Aggiunto avviso in caso di <noscript>

--- app/controllers/contest_controller.rb
+++ app/controllers/contest_controller.rb
@@ -36,6 +36,17 @@ class ContestController < ApplicationController
     UpdateDataJob.perform_now(false)
     return :ok
   end
+  
+  def usercount 
+    @contest = Contest.find(params[:id])
+    count = 0
+      Creator.all.each do |creator|
+        if creator.photos.where(contest: @contest).any?
+          count += 1
+        end
+      end
+      render :json => count
+  end
 end
 private
 def params_ok
