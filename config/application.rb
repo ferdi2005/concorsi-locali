3a9e821f7a5d5fcb3937e32adc0a235432470a94
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-09-09 15:13:12 +0200

    Fix policy Access-Control-Allow-Origin

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Concorsilocali
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'Access-Control-Allow-Headers: Origin'
+    }    
   end
 end
