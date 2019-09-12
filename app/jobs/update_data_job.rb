295a205b211a2cabb75c9cd8b3a305d228cee0ba
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-09-12 21:09:50 +0200

    Modifica temporanea relativa ai nomi utenti contenenti & presenti nell'api di MediaWiki

--- app/jobs/update_data_job.rb
+++ app/jobs/update_data_job.rb
@@ -32,8 +32,9 @@ class UpdateDataJob < ApplicationJob
               globalusage = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&prop=globalusage&pageids=#{photo['pageid']}&gunamespace=0&format=json", uri_adapter: Addressable::URI).to_a[1][1]['pages'][photo['pageid'].to_s]['globalusage'].try(:empty?)
 
               unless (@creator = Creator.find_by(username: photoinfo['user']))
-                creationdate = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&meta=globaluserinfo&guiuser=#{photoinfo['user']}&format=json", uri_adapter: Addressable::URI).to_a[1][1]['globaluserinfo']['registration']
-                @creator = Creator.create(username: photoinfo['user'], userid: photoinfo['userid'], creationdate: creationdate)
+
+                @creationdate = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&meta=globaluserinfo&guiuser=#{photoinfo['user']}&format=json", uri_adapter: Addressable::URI).to_a[1][1]['globaluserinfo']['registration'] unless photoinfo['user'].match?(/&/)
+                @creator = Creator.create(username: photoinfo['user'], userid: photoinfo['userid'], creationdate: @creationdate)
                 @creator.update_attribute(:proveniencecontest, contest.id) if creationdate.to_date == photoinfo['timestamp'].to_date || creationdate.to_date.between?(Date.parse('30/08/2019'), Date.parse('30/09/2019'))
               end
               Photo.create(pageid: photo['pageid'], name: photo['title'], creator: @creator, contest: contest, photodate: photoinfo['timestamp'], usedonwiki: !globalusage)
