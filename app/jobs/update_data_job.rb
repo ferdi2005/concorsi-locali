554e628017f14f595dc928f9ef1048a8468d1c74
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-09-14 18:14:05 +0200

    Blocco in caso di utente == nil

--- app/jobs/update_data_job.rb
+++ app/jobs/update_data_job.rb
@@ -31,13 +31,15 @@ class UpdateDataJob < ApplicationJob
               photoinfo = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&pageids=#{photo['pageid']}&prop=imageinfo&iiprop=user|timestamp|userid&format=json", uri_adapter: Addressable::URI).to_a[1][1]['pages'][photo['pageid'].to_s]['imageinfo'][0] # Looks for photoinfo
               globalusage = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&prop=globalusage&pageids=#{photo['pageid']}&gunamespace=0&format=json", uri_adapter: Addressable::URI).to_a[1][1]['pages'][photo['pageid'].to_s]['globalusage'].try(:empty?)
 
-              unless (@creator = Creator.find_by(username: photoinfo['user']))
+              unless (@creator = Creator.find_by(username: photoinfo['user'])) || photoinfo['user'].nil?
                 photoinfo['user'] = photoinfo['user'].gsub!('&', '%26')
                 @creationdate = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&meta=globaluserinfo&guiuser=#{photoinfo['user']}&format=json", uri_adapter: Addressable::URI).to_a[1][1]['globaluserinfo']['registration']
                 @creator = Creator.create(username: photoinfo['user'], userid: photoinfo['userid'], creationdate: @creationdate)
                 @creator.update_attribute(:proveniencecontest, contest.id) if @creationdate.to_date == photoinfo['timestamp'].to_date || @creationdate.to_date.between?(Date.parse('30/08/2019'), Date.parse('30/09/2019'))
               end
-              Photo.create(pageid: photo['pageid'], name: photo['title'], creator: @creator, contest: contest, photodate: photoinfo['timestamp'], usedonwiki: !globalusage)
+              unless photoinfo['user'] == nil
+                Photo.create(pageid: photo['pageid'], name: photo['title'], creator: @creator, contest: contest, photodate: photoinfo['timestamp'], usedonwiki: !globalusage)
+              end
           end
         end
       end
