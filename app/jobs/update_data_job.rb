e5acf6e248e4d6123947adb8b8ba73eb04303f6a
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-09-24 20:55:46 +0200

    Evito processing di tutte le foto per contest con numero di foto uguale

--- app/jobs/update_data_job.rb
+++ app/jobs/update_data_job.rb
@@ -4,7 +4,7 @@ class UpdateDataJob < ApplicationJob
 
   def perform
     start = Time.now
-    Contest.all.each do |contest|
+    Contest.includes(:photos).each do |contest|
       puts 'Ottengo la categoria...'
       request_photolist = "https://commons.wikimedia.org/w/api.php?action=query&list=categorymembers&cmtitle=#{contest.category}&cmlimit=500&cmdir=newer&format=json"
       photolist = HTTParty.get(request_photolist, uri_adapter: Addressable::URI).to_a
@@ -37,30 +37,32 @@ class UpdateDataJob < ApplicationJob
           break if noph
         end
         puts 'Inizio a processare le singole foto...'
-        photolist.each do |photo|
-          unless Photo.find_by(pageid: photo['pageid'])
-              photoinfo = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&pageids=#{photo['pageid']}&prop=imageinfo&iiprop=user|timestamp|userid&format=json", uri_adapter: Addressable::URI).to_a[1][1]['pages'][photo['pageid'].to_s]['imageinfo'][0] # Looks for photoinfo
-              globalusage = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&prop=globalusage&pageids=#{photo['pageid']}&gunamespace=0&format=json", uri_adapter: Addressable::URI).to_a[1][1]['pages'][photo['pageid'].to_s]['globalusage'].try(:empty?)
-              puts "Foto: #{photo['title']} di #{photoinfo['user']}..."
-              unless (@creator = Creator.find_by(username: photoinfo['user']) || @creator = Creator.find_by(userid: photoinfo['userid']))
-                  photoinfo['user'] = photoinfo['user'].gsub!('&', '%26')
-                unless photoinfo['user'].nil?
-                  begin
-                    @creationdate = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&meta=globaluserinfo&guiuser=#{photoinfo['user']}&format=json", uri_adapter: Addressable::URI).to_a[1][1]['globaluserinfo']['registration']
-                  rescue NoMethodError => e
-                    puts e
-                    # dummy date
-                    @creationdate = '2009-01-01'
+        unless photolist.count == contest.photos.count
+          photolist.each do |photo|
+            unless Photo.find_by(pageid: photo['pageid'])
+                photoinfo = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&pageids=#{photo['pageid']}&prop=imageinfo&iiprop=user|timestamp|userid&format=json", uri_adapter: Addressable::URI).to_a[1][1]['pages'][photo['pageid'].to_s]['imageinfo'][0] # Looks for photoinfo
+                globalusage = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&prop=globalusage&pageids=#{photo['pageid']}&gunamespace=0&format=json", uri_adapter: Addressable::URI).to_a[1][1]['pages'][photo['pageid'].to_s]['globalusage'].try(:empty?)
+                puts "Foto: #{photo['title']} di #{photoinfo['user']}..."
+                unless (@creator = Creator.find_by(username: photoinfo['user']) || @creator = Creator.find_by(userid: photoinfo['userid']))
+                    photoinfo['user'] = photoinfo['user'].gsub!('&', '%26')
+                  unless photoinfo['user'].nil?
+                    begin
+                      @creationdate = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&meta=globaluserinfo&guiuser=#{photoinfo['user']}&format=json", uri_adapter: Addressable::URI).to_a[1][1]['globaluserinfo']['registration']
+                    rescue NoMethodError => e
+                      puts e
+                      # dummy date
+                      @creationdate = '2009-01-01'
+                    end
+                    puts "Creo l'utente #{photoinfo['user']}"
+                    @creator = Creator.create(username: photoinfo['user'], userid: photoinfo['userid'], creationdate: @creationdate)
+                    unless @creationdate.nil?
+                      @creator.update_attribute(:proveniencecontest, contest.id) if @creationdate.to_date == photoinfo['timestamp'].to_date || @creationdate.to_date.between?(Date.parse('30/08/2019'), Date.parse('30/09/2019'))  
+                    end                
                   end
-                  puts "Creo l'utente #{photoinfo['user']}"
-                  @creator = Creator.create(username: photoinfo['user'], userid: photoinfo['userid'], creationdate: @creationdate)
-                  unless @creationdate.nil?
-                    @creator.update_attribute(:proveniencecontest, contest.id) if @creationdate.to_date == photoinfo['timestamp'].to_date || @creationdate.to_date.between?(Date.parse('30/08/2019'), Date.parse('30/09/2019'))  
-                  end                
                 end
-              end
-                Photo.create(pageid: photo['pageid'], name: photo['title'], creator: @creator, contest: contest, photodate: photoinfo['timestamp'], usedonwiki: !globalusage)
-          end
+                  Photo.create(pageid: photo['pageid'], name: photo['title'], creator: @creator, contest: contest, photodate: photoinfo['timestamp'], usedonwiki: !globalusage)
+            end
+        end
       end
       end
     end
