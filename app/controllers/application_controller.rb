bb9529979f2b8694bb915a3212502dae8d873b01
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-09-27 17:24:56 +0200

    Fix nome errato

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
-  unless Date.today.month == 9
-    flash[:info] = "Questi dati non vengono più aggiornati perchè è finito Settembre, rimangono validi. L'aggiornamento finale viene eseguito nel mese di ottobre solo se qualcuno visita la pagina (è stato schedulato e dovrebbe essere eseguito a breve)."
-  end
-  
-  if Date.today.month == 10
-    UpdateDataJob.perform_later
+  before_action :check_date
+  def check_date
+    unless Date.today.month == 9
+      flash[:info] = "Questi dati non vengono più aggiornati perchè è finito Settembre, rimangono validi. L'aggiornamento finale viene eseguito nel mese di ottobre solo se qualcuno visita la pagina (è stato schedulato e dovrebbe essere eseguito a breve)."
+    end
+    
+    if Date.today.month == 10
+      UpdateDataWorker.perform_async
+      flash[:info] = "Verrà iniziato l'aggiornamento manuale dei dati. Ricarica questa pagina tra un po'"
+    end
   end
 
 end
