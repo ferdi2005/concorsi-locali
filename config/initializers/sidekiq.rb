23dfacd9cabf25f5c4a5d8256160b3a6777bfec0
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-09-27 17:11:17 +0200

    Cambio metodo job scheduling

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,6 @@
-Sidekiq::Cron::Job.create(name: 'Update Data Job', cron: '*/20 * * 9 *', class: 'UpdateDataJob')
-Sidekiq::Cron::Job.create(name: 'Update Global Usage', cron: '01 0 * * *', class: 'UpdateGlobalUsageJob')
-Sidekiq::Cron::Job.create(name: 'Update Photos Count Job', cron: '*/5 * * * *', class: 'UpdatePhotosCountJob')
-Sidekiq::Cron::Job.create(name: 'Delete Wrong Creators Job', cron: '*/10 * * * *', class: 'DeleteWrongCreatorsJob')
+#initializers/sidekiq.rb
+schedule_file = "config/schedule.yml"
+
+if File.exist?(schedule_file) && Sidekiq.server?
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 require 'sidekiq/web'
+require 'sidekiq/cron/web'
 Rails.application.routes.draw do
   root 'contest#index'
   resources :contest, only: [:show]
   resources :creators, only: [:show]
   get 'upload', to: 'contest#upload'
   post 'upload', to: 'contest#uploadpost'
-  mount Sidekiq::Web => '/sidekiq'
+  mount Sidekiq::Web => '/secret-sidekiq'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
