92f730de2bac1b6e2162f928ecf8f82390c216f5
Ferdinando Traversa <ferdi.traversa@gmail.com>
2020-09-01 22:04:43 +0200

    Nascondo Sidekiq per sicurezza

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-require 'sidekiq/web'
-require 'sidekiq/cron/web'
+# require 'sidekiq/web'
+# require 'sidekiq/cron/web'
 Rails.application.routes.draw do
   root 'contest#index'
   resources :contest, only: [:show]
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'upload', to: 'contest#upload'
   post 'upload', to: 'contest#uploadpost'
   
-  mount Sidekiq::Web => '/secret-sidekiq'
+  # mount Sidekiq::Web => '/secret-sidekiq'
 
   scope '/numerics' do
     get 'totalphotos', to: 'numerics#totalphotos'
