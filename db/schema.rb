4b399a05ac50dd2331e25dde6661cfa75422adf8
ferdi2005 <ferdi.traversa@gmail.com>
2020-10-02 00:03:28 +0200

    calcolo var percentuale nophoto
    Fix gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_121536) do
+ActiveRecord::Schema.define(version: 2020_10_01_220207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_08_30_121536) do
     t.integer "with_image"
     t.integer "nowlm"
     t.string "region"
+    t.integer "year", default: 2020
   end
 
   create_table "creators", force: :cascade do |t|
@@ -71,6 +72,7 @@ ActiveRecord::Schema.define(version: 2020_08_30_121536) do
     t.string "regione"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "percent"
   end
 
   create_table "photos", force: :cascade do |t|
