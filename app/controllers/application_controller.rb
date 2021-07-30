1500cc9515a2b249692c6be08885f401f19ed86b
ferdi2005 <ferdi.traversa@gmail.com>
2021-07-30 21:35:17 +0200

    Password per caricare logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :check_date
   def check_date
     unless Date.today.month == 9
-      flash[:info] = "Questi dati non vengono più aggiornati perchè è finito Settembre, rimangono validi. L'aggiornamento finale viene eseguito nel mese di ottobre solo se qualcuno visita la pagina (è stato schedulato e dovrebbe essere eseguito a breve)."
+      flash[:info] = "Questi dati non vengono più aggiornati perchè è finito Settembre, rimangono validi. Un aggiornamento viene eseguito nel mese di ottobre qualora qualcuno visiti la pagina."
     end
     
     if Date.today.month == 10
