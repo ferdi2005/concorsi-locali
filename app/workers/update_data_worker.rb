8fd85439dbbf78e21670e5ab38c7fed09ab5d16a
ferdi2005 <ferdi.traversa@gmail.com>
2021-08-06 21:37:41 +0200

    Velocizzo maggiormente l'aggiornamento e l'inserimento

--- app/workers/update_data_worker.rb
+++ app/workers/update_data_worker.rb
@@ -25,47 +25,54 @@ class UpdateDataWorker
 
       # Rimuove le fotografie che già esistono in memoria o che non sono foto (namespace 6)
       photolist.reject! { |_, photo| Photo.exists?(name: photo['title']) || photo['ns'] != 6 }
-      
-      photolist.each do |_, photo|
-        # Shortcut alle informazioni più proprie dell'immagine
-        photoinfo = photo['imageinfo'][0]
 
-        # Procede solo se la fotografia è stata scattata nel periodo di Settembre
-        next unless photoinfo['timestamp'].to_date.between?(Date.parse("1 september #{contest.year}"), Date.parse("30 september #{contest.year}"))
+      @photos_to_be_inserted = []
 
-        # Verifica la presenza o no di utilizzi della fotografia 
-        # (prende il valore contrario poiché con blank si verifica con true l'assenza di utilizzi)
+      photolist.group_by { |_,v| [v['imageinfo'][0]['user'], v['imageinfo'][0]['userid']] }.each do |userinfo, photos|
 
-        usedonwiki = !HTTParty.get("https://commons.wikimedia.org/w/api.php", query: {action: :query, prop: :globalusage, pageids:photo['pageid'], gunamespace: 0, format: :json}, uri_adapter: Addressable::URI).to_h["query"]["pages"][photo['pageid'].to_s]["globalusage"].blank?
-        
+        # Recupero le singole informazioni per l'identificazione
+        username = userinfo[0]
+        userid = userinfo[1]
+
         # Se non c'è l'utente, lo crea, altrimenti lo individua normalmente
-        if Creator.exists?(username: photoinfo['user']) || Creator.exists?(userid: photoinfo['userid'])
-          creator = Creator.where(username: photoinfo['user']).or(Creator.where(userid: photoinfo['userid'])).first
+        if Creator.exists?(username: username) || Creator.exists?(userid: userid)
+          creator = Creator.where(username: username).or(Creator.where(userid: userid)).first
         else
           # Recupera le informazioni sull'utente da Commons
-          userinfo = HTTParty.get("https://commons.wikimedia.org/w/api.php", query: {action: :query, meta: :globaluserinfo, guiuser: photoinfo['user'], format: :json}, uri_adapter: Addressable::URI).to_h["query"]["globaluserinfo"]
+          userinfo = HTTParty.get("https://commons.wikimedia.org/w/api.php", query: {action: :query, meta: :globaluserinfo, guiuser: userid, format: :json}, uri_adapter: Addressable::URI).to_h["query"]["globaluserinfo"]
 
-          creator = Creator.create(username: photoinfo["user"], userid: photoinfo['userid'], creationdate: userinfo['registration'])
+        
+          creator = Creator.create(username: username, userid: userid, creationdate: userinfo['registration'])
 
           # Verifica se l'utente si è iscritto appositamente per il concorso
           unless userinfo['registration'].nil?
-            creator.update_attribute(:proveniencecontest, contest.id) if userinfo['registration'].to_date == photoinfo['timestamp'].to_date || userinfo['registration'].to_date.between?(Date.parse('30 august'), Date.parse('30 september'))
+            creator.update!(proveniencecontest: contest.id) if userinfo['registration'].to_date == photoinfo['timestamp'].to_date || userinfo['registration'].to_date.between?(Date.parse('30 august'), Date.parse('30 september'))
           end
         end
 
-        @photo = Photo.create(pageid: photo['pageid'], name: photo['title'], creator: creator, contest: contest, photodate: photoinfo['timestamp'], usedonwiki: usedonwiki)
+        photos.each do |_, photo|
+          # Shortcut alle informazioni più proprie dell'immagine
+          photoinfo = photo['imageinfo'][0]
+
+          # Procede solo se la fotografia è stata scattata nel periodo di Settembre
+          next unless photoinfo['timestamp'].to_date.between?(Date.parse("1 september #{contest.year}"), Date.parse("30 september #{contest.year}"))
+
+          @photos_to_be_inserted.push({pageid: photo['pageid'], name: photo['title'], creator_id: creator.id, contest_id: contest.id, photodate: photoinfo['timestamp'], created_at: DateTime.now, updated_at: DateTime.now})
+        end
       end
+
+      Photo.insert_all(@photos_to_be_inserted) unless @photos_to_be_inserted.empty?
     end
 
     puts 'Inizio salvataggio del conto dei creatori.'
     Contest.all.each do |contest|
       # Creatori con foto partecipanti
       creators = Creator.includes(:photos).select { |m| m.photos.where(contest: contest).any? }.count
-      contest.update_attribute(:creators, creators)
+      contest.update!(creators: creators)
       
       # Utenti iscritti appositamente per il concorso
       creatorsapposta = Creator.where(proveniencecontest: contest.id).count
-      contest.update_attribute(:creatorsapposta, creatorsapposta) 
+      contest.update!(creatorsapposta: creatorsapposta) 
     end
   end
 end
--- db/migrate/20190903090223_add_creationdate_to_creators.rb
+++ db/migrate/20190903090223_add_creationdate_to_creators.rb
@@ -5,7 +5,7 @@ class AddCreationdateToCreators < ActiveRecord::Migration[6.0]
     Creator.all.each do |creator|
       if creator.creationdate.nil?
         creationdate = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&meta=globaluserinfo&guiuser=#{creator.username}&format=json").to_a[1][1]['globaluserinfo']['registration']
-        creator.update_attribute(:creationdate, creationdate)
+        creator.update!(creationdate: creationdate)
       end
     end     
   end
