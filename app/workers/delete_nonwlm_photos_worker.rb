31bd87c6bb64a3c8e8ae12dda43b53a5d4add539
ferdi2005 <ferdi.traversa@gmail.com>
2021-09-27 11:53:42 +0200

    Correggo job cancellazione foto

--- app/workers/delete_nonwlm_photos_worker.rb
+++ app/workers/delete_nonwlm_photos_worker.rb
@@ -5,9 +5,9 @@ class DeleteNonwlmPhotosWorker
     Photo.all.each do |photo|
       # Verifica che la fotografia sia ancora parte del concorso cercandone i template e trovando quello che identifica una foto partecipante al concorso
 
-      templates = HTTParty.get("https://commons.wikimedia.org/w/api.php", query: {:action=>:query, :prop=>:templates, :pageids=> photo.pageid, :tllimit => :max, :format=>:json}).to_h["query"]["pages"][photo.pageid.to_s]["templates"]
+      templates = HTTParty.get("https://commons.wikimedia.org/w/api.php", query: {:action=>:query, :prop=>:templates, :pageids=> photo.pageid, :tllimit => :max, :format=>:json}).to_h.try(:[], "query").try(:[], "pages").try(:[], photo.pageid.to_s).try(:[], "templates")
 
-      unless templates.any? { |t| t["title"] == "Template:Monumento italiano" }
+      unless templates.try(:any?) { |t| t["title"] == "Template:Monumento italiano" }
         photo.destroy
         if photo.creator.photos.count == 0
           # Se all'autore non sono rimaste fotografie, lo elimino per non lasciarne conto
