ced361f5d7bbcdb8c20b6183b78a9866cf317101
Ferdinando Traversa <ferdi.traversa@gmail.com>
2022-08-29 13:03:33 +0200

    statistiche2

--- config/deploy/statistiche2.rb
+++ config/deploy/statistiche2.rb
@@ -1,4 +1,4 @@
-set :application, "concorsi-locali"
+set :application, "statistiche2"
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :sidekiq_service_unit_name, "#{fetch(:application)}-sidekiq"
@@ -8,7 +8,7 @@ namespace :deploy do
       before :linked_files, :upload_env do
         on roles(:app), in: :sequence, wait: 10 do
             puts "Uploading .env file..."
-            upload! '.env', "#{shared_path}/.env"
+            upload! '.env2', "#{shared_path}/.env"
         end
       end
     end
