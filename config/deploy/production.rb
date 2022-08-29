5a5343990ecffa72f86f07d8e5b0732c5a9b0308
Ferdinando Traversa <ferdi.traversa@gmail.com>
2022-08-30 00:26:50 +0200

    Aggiustamenti deploy statistiche2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,11 @@ set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :sidekiq_service_unit_name, "#{fetch(:application)}-sidekiq"
 
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+
 namespace :deploy do
     namespace :check do
       before :linked_files, :upload_env do
