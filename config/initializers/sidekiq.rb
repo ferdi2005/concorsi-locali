7118d0371f911486a1de4f17c0dbedaaffebb621
Ferdinando Traversa <ferdi.traversa@gmail.com>
2024-08-30 12:47:19 +0200

    Azione di load manuale superflua (

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,2 @@
 #initializers/sidekiq.rb
-schedule_file = "config/schedule.yml"
-
-if File.exist?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-end
+# La gem carica in automatico la schedule da config/schedule.yml
