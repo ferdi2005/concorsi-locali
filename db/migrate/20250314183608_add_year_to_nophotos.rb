3cab43fc688f2cdf77292a18235e93c598674cc2
ferdi2005 <ferdi.traversa@gmail.com>
2025-03-15 22:49:45 +0100

    Rimuovo costraint che dà problemi in migration

--- db/migrate/20250314183608_add_year_to_nophotos.rb
+++ db/migrate/20250314183608_add_year_to_nophotos.rb
@@ -1,5 +1,5 @@
 class AddYearToNophotos < ActiveRecord::Migration[6.1]
   def change
-    add_reference :nophotos, :year, null: false, foreign_key: true
+    add_reference :nophotos, :year, foreign_key: true
   end
 end
