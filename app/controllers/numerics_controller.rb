2333d518c3b871871d55101eb2a8b2f699b3bd2e
Ferdinando Traversa <ferdi.traversa@gmail.com>
2025-09-03 18:41:42 +0200

    Statistiche numerics

--- app/controllers/numerics_controller.rb
+++ app/controllers/numerics_controller.rb
@@ -1,11 +1,13 @@
 class NumericsController < ApplicationController
     before_action :set_contest
+    before_action :set_year
 
     def totalphotos
         unless @contest == false
-            photocount = @contest.count
+            @contestyear = ContestYear.find_by(contest: @contest, year: @year)
+            photocount = @contestyear&.count || 0
         else
-            photocount = Photo.count
+            photocount = @year.total || 0
         end
         json = {
             'postfix': 'Fotografie totali',
@@ -21,12 +23,12 @@ class NumericsController < ApplicationController
     def todayphotos
         data = []
         unless @contest == false
-            photowithdate = @contest.photos.group_by_day(:photodate).count.to_a.last(2).reverse
+            photowithdate = @contest.photos.where(year: @year).group_by_day(:photodate).count.to_a.last(2).reverse
             photowithdate.each do |ph|
                 data.push({'value': ph[1]})
             end
         else
-            photowithdate = Photo.all.group_by_day(:photodate).count.to_a.last(2).reverse
+            photowithdate = Photo.where(year: @year).group_by_day(:photodate).count.to_a.last(2).reverse
             photowithdate.each do |ph|
                 data.push({'value': ph[1]})
             end
@@ -42,9 +44,10 @@ class NumericsController < ApplicationController
 
     def totalcreators
         unless @contest == false
-            creatorcount = @contest.creators
+            @contestyear = ContestYear.find_by(contest: @contest, year: @year)
+            creatorcount = @contestyear&.creators || 0
         else
-            creatorcount = Creator.count
+            creatorcount = @year.creators || 0
         end
         json = {
             'postfix': 'Partecipanti',
@@ -59,9 +62,10 @@ class NumericsController < ApplicationController
 
     def totalcreatorsapposta
         unless @contest == false
-            creatorcount = @contest.creatorsapposta.to_i
+            @contestyear = ContestYear.find_by(contest: @contest, year: @year)
+            creatorcount = @contestyear&.creatorsapposta&.to_i || 0
         else
-            creatorcount = Creator.where.not(proveniencecontest: nil).count
+            creatorcount = Creator.where.not(proveniencecontest: nil).where(year: @year).count
         end
             json = {
                 'postfix': 'Partecipanti iscritti per il concorso',
@@ -77,12 +81,12 @@ class NumericsController < ApplicationController
     def todaycreatorsapposta
         data = []
         unless @contest == false
-            photowithdate = Creator.where(proveniencecontest: @contest.id).group_by_day(:creationdate).count.to_a.last(2).reverse
+            photowithdate = Creator.where(proveniencecontest: @contest.id, year: @year).group_by_day(:creationdate).count.to_a.last(2).reverse
             photowithdate.each do |ph|
                 data.push({'value': ph[1]})
             end
         else
-            photowithdate = Creator.all.group_by_day(:creationdate).count.to_a.last(2).reverse
+            photowithdate = Creator.where(year: @year).group_by_day(:creationdate).count.to_a.last(2).reverse
             photowithdate.each do |ph|
                 data.push({'value': ph[1]})
             end
@@ -100,10 +104,10 @@ class NumericsController < ApplicationController
     def days
         if Date.today.month == 9
             name = "Fine di WLM"
-            date = Date.parse('30 september').strftime("%Y-%m-%d")
+            date = Date.parse("30 september #{@year.year}").strftime("%Y-%m-%d")
         else
             name = "Inizio di WLM"
-            date = Date.parse('1 september').strftime("%Y-%m-%d")
+            date = Date.parse("1 september #{@year.year}").strftime("%Y-%m-%d")
         end
         respond_to do |format|
             format.json { render json: {
@@ -119,11 +123,11 @@ class NumericsController < ApplicationController
     def photograph
         hashdata = []
         unless @contest == false
-            @contest.photos.group_by_day(:photodate).count.to_a.last(31).each do |pl|
+            @contest.photos.where(year: @year).group_by_day(:photodate).count.to_a.last(31).each do |pl|
                 hashdata.push({"value": pl[1]})
             end
         else
-            Photo.all.group_by_day(:photodate).count.to_a.last(31).each do |pl|
+            Photo.where(year: @year).group_by_day(:photodate).count.to_a.last(31).each do |pl|
                 hashdata.push({"value": pl[1]})
             end
         end
@@ -139,11 +143,11 @@ class NumericsController < ApplicationController
     def photoday
         hashdata = []
         unless @contest == false
-            @contest.photos.group_by_day(:photodate).count.to_a.last(31).each do |pl|
+            @contest.photos.where(year: @year).group_by_day(:photodate).count.to_a.last(31).each do |pl|
                 hashdata.push({"date": pl[0].strftime("%Y-%m-%d"), "value": pl[1]})
             end
         else
-            Photo.all.group_by_day(:photodate).count.to_a.last(31).each do |pl|
+            Photo.where(year: @year).group_by_day(:photodate).count.to_a.last(31).each do |pl|
                 hashdata.push({"date": pl[0].strftime("%Y-%m-%d"), "value": pl[1]})
             end
         end
@@ -158,9 +162,10 @@ class NumericsController < ApplicationController
 
     def usedonwiki
         unless @contest == false
-            photocount = @contest.photos.where(usedonwiki: true).count
+            @contestyear = ContestYear.find_by(contest: @contest, year: @year)
+            photocount = @contestyear&.usedonwiki || 0
         else
-            photocount = Photo.where(usedonwiki: true).count
+            photocount = Photo.where(usedonwiki: true, year: @year).count
         end
         json = {
             'postfix': 'Fotografie usate sui progetti',
@@ -175,11 +180,12 @@ class NumericsController < ApplicationController
 
     def usedonwikipercentage
         unless @contest == false
-            photocount = (@contest.photos.where(usedonwiki: true).count.to_f / @contest.count.to_f * 100.0).truncate(1) unless (@contest.photos.where(usedonwiki: true).count.to_f / @contest.count.to_f * 100.0).nan?
-            photocount = '0.0' if (@contest.photos.where(usedonwiki: true).count.to_f / @contest.count.to_f * 100.0).nan?
+            @contestyear = ContestYear.find_by(contest: @contest, year: @year)
+            photocount = @contestyear&.usedonwiki_percentage&.truncate(1) || '0.0'
         else
-            photocount = (Photo.where(usedonwiki: true).count.to_f / Photo.all.count.to_f * 100.0).truncate(1) unless (Photo.where(usedonwiki: true).count.to_f / Photo.all.count.to_f * 100.0).nan?
-            photocount = '0.0' if (Photo.where(usedonwiki: true).count.to_f / Photo.all.count.to_f * 100.0).nan?
+            total_photos = @year.total
+            used_photos = @year.photos.where(usedonwiki: true).count
+            photocount = total_photos > 0 ? (used_photos.to_f / total_photos.to_f * 100.0).truncate(1) : '0.0'
         end
         json = {
             'postfix': 'Fotografie usate sui progetti',
@@ -195,8 +201,10 @@ class NumericsController < ApplicationController
 
     def photos_relativetogeneral
         unless @contest == false
-            value = (@contest.count.to_f / Photo.count.to_f * 100).truncate(1) unless (@contest.count.to_f / Photo.count.to_f * 100.0).nan?
-            value = "0" if (@contest.count.to_f / Photo.count.to_f * 100.0).nan?
+            @contestyear = ContestYear.find_by(contest: @contest, year: @year)
+            contest_count = @contestyear&.count || 0
+            total_count = @year.total || 1
+            value = contest_count > 0 && total_count > 0 ? (contest_count.to_f / total_count.to_f * 100).truncate(1) : "0"
         else
             value = "100"
         end
@@ -213,8 +221,10 @@ class NumericsController < ApplicationController
 
     def participants_relativetogeneral
         unless @contest == false
-            value = (@contest.creators.to_f / Creator.count.to_f * 100).truncate(1) unless (@contest.creators.to_f / Creator.count.to_f * 100.0).nan?
-            value = "0" if (@contest.creators.to_f / Creator.count.to_f * 100.0).nan?
+            @contestyear = ContestYear.find_by(contest: @contest, year: @year)
+            contest_creators = @contestyear&.creators || 0
+            total_creators = @year.creators || 1
+            value = contest_creators > 0 && total_creators > 0 ? (contest_creators.to_f / total_creators.to_f * 100).truncate(1) : "0"
         else
             value = "100"
         end
@@ -231,12 +241,13 @@ class NumericsController < ApplicationController
 
     def special_photos
         unless @contest == false
-            photocount = @contest.special_category_count
+            @contestyear = ContestYear.find_by(contest: @contest, year: @year)
+            photocount = @contestyear&.special_category_count || 0
         else
-            photocount = "0"
+            photocount = @year.special_category_total || 0
         end
         json = {
-            'postfix': "#{ENV["SPECIAL_CATEGORY_LABEL"]}",
+            'postfix': "#{@year.special_category_label || ENV["SPECIAL_CATEGORY_LABEL"]}",
             'data': {
                 "value": photocount
              }
@@ -249,9 +260,10 @@ class NumericsController < ApplicationController
 
     def new_monuments
         unless @contest == false
-            photocount = @contest.photos.select { |p| p.new_monument == true }.count
+            @contestyear = ContestYear.find_by(contest: @contest, year: @year)
+            photocount = @contestyear&.new_monuments || 0
         else
-            photocount = Photo.select { |p| p.new_monument == true }.count
+            photocount = @year.new_monuments || 0
         end
         json = {
             'postfix': "Nuovi monumenti",
@@ -267,11 +279,14 @@ class NumericsController < ApplicationController
 
     def iscrittiappostapercentage
         unless @contest == false
-            photocount = (@contest.creatorsapposta.to_f / @contest.creators.to_f * 100.0).truncate(1) unless (@contest.creatorsapposta.to_f / @contest.creators.to_f * 100.0).nan? || @contest.creatorsapposta == 0 || @contest.creators == 0
-            photocount = '0.0' if (@contest.creatorsapposta.to_f / @contest.creators.to_f * 100.0).nan? || @contest.creatorsapposta == 0 || @contest.creators == 0
+            @contestyear = ContestYear.find_by(contest: @contest, year: @year)
+            creatorsapposta = @contestyear&.creatorsapposta&.to_f || 0
+            creators = @contestyear&.creators&.to_f || 0
+            photocount = creators > 0 ? (creatorsapposta / creators * 100.0).truncate(1) : '0.0'
         else
-            photocount = (Creator.where.not(proveniencecontest: nil).count.to_f / Creator.count.to_f * 100.0).truncate(1) unless (Creator.where.not(proveniencecontest: nil).count.to_f / Creator.count.to_f * 100.0).nan? || Creator.where.not(proveniencecontest: nil).count == 0 || Creator.count == 0
-            photocount = '0.0' if (Creator.where.not(proveniencecontest: nil).count.to_f / Creator.count.to_f * 100.0).nan? || Creator.where.not(proveniencecontest: nil).count == 0 || Creator.count == 0
+            total_creators = Creator.where(year: @year).count.to_f
+            creatorsapposta = Creator.where.not(proveniencecontest: nil).where(year: @year).count.to_f
+            photocount = total_creators > 0 ? (creatorsapposta / total_creators * 100.0).truncate(1) : '0.0'
         end
         json = {
             'postfix': 'Percentuale',
@@ -288,20 +303,20 @@ class NumericsController < ApplicationController
         unless @contest == false
             list = []
             @creators = []
-            Creator.all.each do |creator|
-              if creator.photos.where(contest: @contest).any?
+            Creator.where(year: @year).each do |creator|
+              if creator.photos.where(contest: @contest, year: @year).any?
                 @creators.push(creator)
               end
             end
-            @creators = @creators.sort_by{|gp| gp.photos.where(contest: @contest).count}.reverse
+            @creators = @creators.sort_by{|gp| gp.photos.where(contest: @contest, year: @year).count}.reverse
             @creators.each do |creator|
-                list.push({'name': creator.username, 'value': creator.photos.where(contest: @contest).count})
+                list.push({'name': creator.username, 'value': creator.photos.where(contest: @contest, year: @year).count})
             end
         else
             list = []
-            @creators = Creator.all.sort_by{|creator| creator.photos.count}.reverse
+            @creators = Creator.where(year: @year).sort_by{|creator| creator.photos.where(year: @year).count}.reverse
             @creators.each do |creator|
-                list.push({'name': creator.username, 'value': creator.photos.count})
+                list.push({'name': creator.username, 'value': creator.photos.where(year: @year).count})
             end
         end
         json =  {
@@ -317,11 +332,11 @@ class NumericsController < ApplicationController
     def iscrittiappostagraph
         hashdata = []
         unless @contest == false
-            Creator.where(proveniencecontest: @contest.id).group_by_day(:creationdate).count.to_a.last(31).each do |pl|
+            Creator.where(proveniencecontest: @contest.id, year: @year).group_by_day(:creationdate).count.to_a.last(31).each do |pl|
                 hashdata.push({"value": pl[1]})
             end
         else
-            Creator.all.where.not(proveniencecontest: nil).all.group_by_day(:creationdate).count.to_a.last(31).each do |pl|
+            Creator.where.not(proveniencecontest: nil).where(year: @year).group_by_day(:creationdate).count.to_a.last(31).each do |pl|
                 hashdata.push({"value": pl[1]})
             end
         end
@@ -340,5 +355,17 @@ def set_contest
         @contest = Contest.find(params[:id])
     else
         @contest = false
+    end
+end
+
+def set_year
+    if params[:year].present? && (year = Year.find_by(year: params[:year]))
+        @year = year
+    elsif !cookies[:year_id].blank? && Year.find_by(id: cookies[:year_id].to_i)
+        @year = Year.find(cookies[:year_id].to_i)
+    elsif (@year = Year.find_by_year(Date.today.year))
+        cookies[:year_id] = @year.id
+    else
+        @year = Year.last
     end
 end
