573572e8f93fd9aa9f9441c0649feb8c1d67ad77
Ferdinando Traversa <ferdi.traversa@gmail.com>
2026-09-07 13:00:30 +0200

    Adattamento per mysql

--- app/workers/download_no_photo_monuments.rb
+++ app/workers/download_no_photo_monuments.rb
@@ -7,6 +7,12 @@ class DownloadNoPhotoMonuments
     HTTParty.get("https://cerca.wikilovesmonuments.it/monuments/nophoto_endpoint.json").each do |monument|
       @no_photo_monuments.push({item: monument[0], wlmid: monument[1], created_at: DateTime.now, updated_at: DateTime.now})
     end
-    NoPhotoMonument.upsert_all(@no_photo_monuments, unique_by: :item) unless @no_photo_monuments.empty?
+    return if @no_photo_monuments.empty?
+
+    if NoPhotoMonument.connection.supports_insert_conflict_target?
+      NoPhotoMonument.upsert_all(@no_photo_monuments, unique_by: :item)
+    else
+      NoPhotoMonument.upsert_all(@no_photo_monuments)
+    end
   end
 end
